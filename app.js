56cb0934c9f56ec8564477731bffab4dea3a86b4
beezay bohora <bijay.okur@gmail.com>
2021-02-24 13:37:10 +0545

    Delete Functionality added

--- app.js
+++ app.js
@@ -14,27 +14,13 @@ class Books {
 //* UI Class: Handle UI Tasks
 class UI {
     static displayBooks() {
-        const StoredBooks = [{
-                title: 'Book one',
-                author: 'John Doe',
-                isbn: '343434'
-            },
-            {
-                title: 'Book Two',
-                author: 'Jane Doe',
-                isbn: '454545'
-            }, {
-                title: 'Book one',
-                author: 'John Doe',
-                isbn: '343434'
-            },
-            {
-                title: 'Book Two',
-                author: 'Jane Doe',
-                isbn: '454545'
-            }
-        ];
-        const books = StoredBooks;
+        // const StoredBooks = [{
+        //         title: 'Book one',
+        //         author: 'John Doe',
+        //         isbn: '343434'
+        //     }
+        // ];
+        const books = Store.getBooks();
         console.log('Books=>', books);
         books.forEach(book => (
             UI.addBookToList(book)
@@ -68,13 +54,60 @@ class UI {
         //* Vanish after sometime
         setTimeout(() => {
             document.querySelector('.alert').remove();
-        },2000);
+        }, 2000);
     }
 
     static clearFields() {
         document.querySelector('#title').value = ''
         document.querySelector('#author').value = ''
         document.querySelector('#isbn').value = ''
+    }
+
+    static editBook(event) {
+        if (event.classList.contains('edit')) {
+            //* Getting Previous Values
+            const titleVal = event.parentElement.parentElement.firstElementChild.innerHTML;
+            const authorVal = event.parentElement.parentElement.firstElementChild.nextElementSibling.innerHTML;
+            const isbnVal = event.parentElement.parentElement.firstElementChild.nextElementSibling.nextElementSibling.innerHTML;
+
+            //* Setting Value to the Form 
+            document.querySelector('#title').value = titleVal
+            document.querySelector('#author').value = authorVal
+            document.querySelector('#isbn').value = isbnVal
+            document.querySelector('.btn').value = 'Update Book'
+
+            //* Updating the New values
+            document.querySelector('#book-form').addEventListener('submit', e => {
+                e.preventDefault();
+                const title = document.querySelector('#title').value;
+                const author = document.querySelector('#author').value;
+                const isbn = document.querySelector('#isbn').value;
+
+                //* Validate Forms
+                if (title === '' || author === '' || isbn === '') {
+                    UI.showAlert('Please fill in all fields', 'danger')
+                } else {
+                    //* Updating DOM with Updated Values
+                    event.parentElement.parentElement.innerHTML = `
+                    <td>${title}</td>
+                    <td>${author}</td>
+                    <td>${isbn}</td>
+                    <td><a href="#" class="btn btn-info edit">Edit</a></td>
+                    <td><a href="#" class="btn btn-danger delete">X</a></td>
+                    `;
+
+                    //* Show Success message
+                    UI.showAlert('Book Details Updated', 'info')
+
+                    //* Clear UI FORM FIELDS
+                    UI.clearFields();
+
+                    console.log(book);
+                }
+
+            })
+            console.log('Edit Button Clicked', event.parentElement.parentElement.innerHTML);
+        }
     }
 
     static deleteBook(event) {
@@ -87,11 +120,11 @@ class UI {
 
 //* Store Class: Handles Storage
 class Store {
-    static getBooks () {
+    static getBooks() {
         let books;
-        if(localStorage.getItem('books') === null){
+        if (localStorage.getItem('books') === null) {
             books = [];
-        }else {
+        } else {
             books = JSON.parse(localStorage.getItem('books'))
         }
 
@@ -105,9 +138,10 @@ class Store {
     }
 
     static removeBook(isbn) {
-        const books = Store.getBooks( );
+        const books = Store.getBooks();
+        console.log(isbn);
         books.forEach((book, index) => {
-            if(book.isbn === isbn) {
+            if (book.isbn === isbn) {
                 books.splice(index, 1);
             }
         });
@@ -138,6 +172,9 @@ document.querySelector('#book-form').addEventListener('submit', e => {
         //* Add Book to UI
         UI.addBookToList(book);
 
+        //* Add Book to Store
+        Store.addBook(book)
+
         //* Show Success message
         UI.showAlert('New Book Added', 'success')
 
@@ -149,9 +186,17 @@ document.querySelector('#book-form').addEventListener('submit', e => {
 
 })
 
+// //* Edit Book Details
+// document.querySelector('#book-list').addEventListener('click', e => {
+//     UI.editBook(e.target);
+// });
 
 //* Event: Delete Book
 document.querySelector('#book-list').addEventListener('click', e => {
+
+    //* Remove Book from UI
     UI.deleteBook(e.target);
-    
+
+    //* Remove Book from Store
+    Store.removeBook(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
 })
